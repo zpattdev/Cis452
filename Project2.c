91ec20abc14d757c67159b0c4bf767da9604eef1
Zackary Patterson <zpattdev@gmail.com>
2016-10-06 03:14:43 -0400

    Signaling is fully functional; sorts integers by lowest

--- Project2.c
+++ Project2.c
@@ -5,35 +5,40 @@
 #include <wait.h>
 #include <sys/types.h>
 #include <math.h>
+#include <time.h>
 
 #define READ 0 
 #define WRITE 1 
 #define MAX 1024
 #define BUFSIZE 2048
+#define FALSE 0
+#define TRUE 1
+#define TESTFILE 8
 
 struct record {
-  int RWflag;		//flag whether record is ready for read or write
-  char msg[100];	//message to test piping
+  int testVal;		//value to test compare
+  char msg[BUFSIZE-5];	//message to test piping
 };
-
 
 pid_t forkCheck(void);
 void pipeCheck (int fd[]);
 int calcPathId (int path[], int len);
-void startMaster (int readLeft, int readRight);
-void startMerger (int readLeft, int readRight, int writeParent);
-void startSorter (int pathId, int writeParent);
+void wakeupHandler();
+void noticeHandler();
+void startMaster (pid_t leftChild, pid_t rightChild);
+void startMerger (pid_t leftChild, pid_t rightChild, int level);
+void startSorter (int pathId);
 
+int readyChildren = 0, readyParent = TRUE;
 int fdLeft[2], fdRight[2], fdParent[2];
 
 int main(int argc, char* argv[]) 
 { 
   pid_t pid1, pid2; 
   //int fdLeft[2], fdRight[2], fdParent[2];
-  int fileNum = 4, fileMax = 4;
+  int fileNum = TESTFILE, fileMax = TESTFILE;
   int pathLen = (int) log2((double) fileNum);
   int pathTracker[pathLen];
-  
   
   while(1){   
     //sleep(1);
@@ -43,29 +48,34 @@ int main(int argc, char* argv[])
     pid1 = forkCheck();
     
     //if parent, need to create right child; else skip
-    if(pid1 > 0) { //parent w/ 1
+    if(pid1 > 0) { //parent w/ 1 child
       //create pipe for right child
       pipeCheck(fdRight);
       //create right child
       pid2 = forkCheck();
       if (pid2 > 0) { //same parent w/ 2 
+	signal(SIGUSR1, wakeupHandler);
+	signal(SIGUSR2, noticeHandler);
 	if (fileNum >= fileMax)
-	  startMaster(fdLeft[READ], fdRight[READ]);
+	  startMaster(pid1, pid2);
 	else 
-	  startMerger(fdLeft[READ], fdRight[READ], fdParent[WRITE]);	
+	  startMerger(pid1, pid2, fileNum);	
       } else { //Right child 
 	pathTracker[(int) log2((double) fileNum) - 1] = 1;
 	fdParent[WRITE] = fdRight[WRITE];
+	signal(SIGUSR1, wakeupHandler);
       }
     } else { //Left child 
       pathTracker[(int) log2((double) fileNum) - 1] = 0;
       fdParent[WRITE] = fdLeft[WRITE];
+      signal(SIGUSR1, wakeupHandler);
     }
     //applies to both children
+    signal(SIGUSR1, wakeupHandler);
     fileNum /= 2;
     if(fileNum < 2)
       //printf("%d\n", fdParent[WRITE]
-      startSorter(calcPathId (pathTracker, pathLen), fdParent[WRITE]);
+      startSorter(calcPathId (pathTracker, pathLen));
   }
   return 0;
 }
@@ -95,48 +105,119 @@ int calcPathId (int path[], int len) {
   return id + 1;
 }
 
-void startMaster (int readLeft, int readRight) {
+void wakeupHandler(int sigNum) {
+  readyParent = TRUE;
+}
+
+void noticeHandler(int sigNum) {
+  readyChildren++;
+  
+}
+
+void startMaster (pid_t leftChild, pid_t rightChild) {
   //printf("Master Ready\n");
-  char testStr[100] = "faq";
+  //char testStr[BUFSIZE] = "faq";
+  struct record leftRec, rightRec;
+  int leftEmpty = TRUE, rightEmpty = TRUE, first = TRUE;
   
   while (1) {
-    read(readLeft, testStr, 100);
-    printf("-----Master gets: %s\n", testStr);
-    
-    read(readRight, testStr, 100);
-    printf("-----Master gets: %s\n", testStr);
     //sleep(1);
+    //if previous read was not Right child
+    //printf("Master has %d children ready\n", readyChildren);
+    if (readyChildren >= 2 && readyParent) {
+      if (leftEmpty && !first){
+	printf("Master reads left\n");
+	read(fdLeft[READ], (void *) &leftRec, BUFSIZE);
+	printf("---Master gets: %s\n", leftRec.msg);
+      }
+      //if previous read was not Left child
+      if (rightEmpty && !first) {
+	printf("Master reads right\n");
+	read(fdRight[READ], (void *) &rightRec, BUFSIZE); 
+	printf("---Master gets: %s\n", rightRec.msg);
+      }
+      readyChildren = 1;
+      if(leftEmpty) {
+	kill(leftChild,SIGUSR1);
+	printf("signal to left Merger\n");
+      }
+      if(rightEmpty) {
+	kill(rightChild,SIGUSR1);
+	printf("signal to right Merger\n");
+      }
+    }
+    first = FALSE;
+    pause();
   }
-  pause();
 }
 
-void startMerger (int readLeft, int readRight, int writeParent) {
+void startMerger (pid_t leftChild, pid_t rightChild, int level) {
   //printf ("Merger Ready\n");
-  char testStr[100] = "faq";
+  //char testStrL[BUFSIZE] = "faqL", testStrR[BUFSIZE] = "faqR";
+  struct record leftRec, rightRec, lowerRec;
+  int leftEmpty = TRUE, rightEmpty = TRUE, first = TRUE;
   
   while(1) {
-    read(readLeft, testStr, 100);
-    printf("---Merger gets: %s\n", testStr);
-    
-    read(readRight, testStr, 100);
-    printf("---Merger gets: %s\n", testStr);
-    
-    write(fdParent[WRITE], testStr, 100);
+    if (readyChildren >= 2 && readyParent) {
+      if (leftEmpty && !first){
+	//printf("start left read\n");
+	read(fdLeft[READ], (void *) &leftRec, BUFSIZE);
+	leftEmpty = FALSE;
+	printf("---Merger %d gets: %s\n", level, leftRec.msg);
+      }
+      if (rightEmpty && !first) {
+	//printf("start right read\n");
+	read(fdRight[READ], (void *) &rightRec, BUFSIZE); 
+	printf("---Merger %d gets: %s\n", level, rightRec.msg);
+	rightEmpty = FALSE;
+      }
+      
+      if (leftRec.testVal < rightRec.testVal) {
+	lowerRec = leftRec;
+	leftEmpty = TRUE;
+      } else {
+	lowerRec = rightRec;
+	rightEmpty = TRUE;
+      }
+
+      printf("Merger sends %s\n", lowerRec.msg);
+      write(fdParent[WRITE], (void *) &lowerRec, BUFSIZE);
+      kill(getppid(),SIGUSR2);
+      readyParent = FALSE;
+
+      
+      readyChildren = 1;
+      if(leftEmpty) kill(leftChild,SIGUSR1);
+      if(rightEmpty) kill(rightChild,SIGUSR1);
+    }
+    first = FALSE;
+    pause();
     //sleep(1);
   }
-  pause();
 }
 
-void startSorter (int pathId, int writeParent) {
+void startSorter (int pathId) {
   //printf("Sorter %d Ready\n",pathId);
-  char testStr[100];
-  while (pathId < 10) {
-    sprintf(testStr, "%d", pathId);
+  int count = 0;
+  struct record writeRec;
+    srand(pathId);
+  //char testStr[BUFSIZE];
+  
+  while (count < 5) {
+    int r = rand() % 10;
+    writeRec.testVal = r;
+    sprintf(writeRec.msg, "Sorter %d sends %d", pathId, r);
     //printf("Sending: %s\n", testStr);
-    write(writeParent, testStr, 100);
-    pathId++;
+    write(fdParent[WRITE], (void *) &writeRec, BUFSIZE);
+    kill(getppid(),SIGUSR2);
+    //printf("Sorter %d waiting\n", pathId);
+    pause();
+    //printf("Sorter %d received signal\n", pathId);
+    
+    count++;
     //sleep(1);
   }
+  printf("limit reached\n");
   pause();
 }
 
